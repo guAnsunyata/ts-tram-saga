8e3ebb7685241b88316a20d6fae9009c1e520d78
guansunyata <guansunyata@gmail.com>
2020-08-20 02:16:19 +0800

    Refactor create order example test

--- __tests__/example/index.test.ts
+++ __tests__/example/index.test.ts
@@ -3,42 +3,45 @@ import { Order } from '@/../example/create-order/entity/Order'
 import { ReserveCredit } from '@/../example/reserve-credit/ReserveCredit'
 import { CustomerNotFound } from '@/../example/reserve-credit/exception/CustomerNotFound'
 
-let result: any = null
-
 // Order
-const approve = jest.fn()
-const reject = jest.fn()
+const orderApprove = jest.fn()
+const orderReject = jest.fn()
 jest.mock('@/../example/create-order/entity/Order')
 ;(Order as jest.Mock).mockImplementation(() => {
   return {
-    approve,
-    reject,
+    approve: orderApprove,
+    reject: orderReject,
   }
 })
 
 // ReserveCredit
+const reserveResult = jest.fn()
 jest.mock('@/../example/reserve-credit/ReserveCredit')
 ;(ReserveCredit as jest.Mock).mockImplementation(() => {
   return {
-    reserve: () => {
-      return result
-    },
+    reserve: reserveResult,
   }
 })
-test('Approve order', async () => {
-  result = Promise.resolve(true)
-  const app = new Application()
-  await app.createOrder()
-  expect(approve).toBeCalled()
-  expect(reject).not.toBeCalled()
-  approve.mockClear()
-  reject.mockClear()
+
+afterEach(() => {
+  orderApprove.mockClear()
+  orderReject.mockClear()
 })
 
-test('Reject order with customer not found', async () => {
-  result = Promise.reject(new CustomerNotFound())
+test('Approve order when credit reserved', async () => {
+  reserveResult.mockResolvedValue(true)
   const app = new Application()
   await app.createOrder()
-  expect(approve).not.toBeCalled()
-  expect(reject).toBeCalled()
+
+  expect(orderApprove).toBeCalled()
+  expect(orderReject).not.toBeCalled()
+})
+
+test('Reject order when giving customer not found rejection', async () => {
+  reserveResult.mockRejectedValue(new CustomerNotFound())
+  const app = new Application()
+  await app.createOrder()
+
+  expect(orderApprove).not.toBeCalled()
+  expect(orderReject).toBeCalled()
 })
